9d1f4427e5ae5ba4663ff6dce5a39ced8746dce4
Majid-dev <majid0karimi@gmail.com>
2026-06-22 10:01:43 +0330

    update formatter

---
+++ src/ui/formatter.py
@@ -0,0 +1,21 @@
+class Formatter:
+
+    @staticmethod
+    def title(text: str):
+
+        return text
+
+    @staticmethod
+    def subtitle(text: str):
+
+        return text
+
+    @staticmethod
+    def success(text: str):
+
+        return text
+
+    @staticmethod
+    def error(text: str):
+
+        return text
